0c51e18c849a8ccca305b07d0b3c27dc7d1ed6fc
andrea <andreacaparrotta@outlook.com>
2022-05-28 12:31:22 +0200

    Delete response.h

--- source/response.h
+++
@@ -1,4 +0,0 @@
-//Definizione dei messaggi di risposta
-#define SUCCESS "0"                                 //L'operazione è terminata con successo
-#define ALREADY_OPENED "1"                          //Il file è stato già aperto
-#define FILE_NOT_EXIST "2"                          //Il file specificato non esiste
